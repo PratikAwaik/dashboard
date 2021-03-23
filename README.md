5b3f3bc0c0f2da7dbc36d9db6a32297d6947bc05
PratikAwaik <pratikawaik125@gmail.com>
2021-03-23 20:14:01 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dashboard
+
+A simple dashboard made in HTML and CSS. Mobile responsiveness coming soon.
---
+++ app.js
@@ -0,0 +1,10 @@
+const searchBtn = document.querySelector(".search-btn");
+const updateProfileBtn = document.querySelector(".update-profile-btn");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+updateProfileBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
